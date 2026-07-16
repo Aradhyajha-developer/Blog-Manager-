b57d05845dd4c8d4b4eb2e1156462342a15f6cf5
Aradhya <26aradhyajha@gmail.com>
2026-07-16 11:52:29 +0530

    Add localStorage support for blog persistence

--- blog/src/js/data.js
+++ blog/src/js/data.js
@@ -1,17 +1,24 @@
-// Store all blogs
-let blogs = [];
+// Load blogs from localStorage
+let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
 
 // Return all blogs
 export function getBlogs() {
   return blogs;
 }
 
-// Add a new blog
-export function addBlog(blog) {
-  blogs.push(blog);
+// Save blogs
+function saveBlogs() {
+  localStorage.setItem("blogs", JSON.stringify(blogs));
 }
 
-// Delete a blog by id
+// Add Blog
+export function addBlog(blog) {
+  blogs.push(blog);
+  saveBlogs();
+}
+
+// Delete Blog
 export function deleteBlog(id) {
   blogs = blogs.filter((blog) => blog.id !== id);
+  saveBlogs();
 }
